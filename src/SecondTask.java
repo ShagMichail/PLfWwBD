f504c90b64aa9d3b5730a9e8b41ed86496e36a7e
ShagMichail <shiga2001@mail.ru>
2024-02-21 18:13:52 +0300

    Fix warnings

--- src/SecondTask.java
+++ src/SecondTask.java
@@ -13,7 +13,7 @@ public class SecondTask {
         input = new Scanner(System.in);
         System.out.println("Введите размер массива: ");
         int size = input.nextInt(); // Читаем с клавиатуры размер массива и записываем в size
-        int array[] = new int[size]; // Создаём массив int размером в size
+        int[] array = new int[size]; // Создаём массив int размером в size
         System.out.println("Введите элементы массива:");
         /*Пройдёмся по всему массиву, заполняя его*/
         for (int i = 0; i < size; i++) {
@@ -40,7 +40,7 @@ public class SecondTask {
         inputSecond = new Scanner(System.in);
         System.out.println("Введите размер массива: ");
         int sizeSecond = input.nextInt(); // Читаем с клавиатуры размер массива и записываем в size
-        int arraySecond[] = new int[sizeSecond]; // Создаём массив int размером в size
+        int[] arraySecond = new int[sizeSecond]; // Создаём массив int размером в size
         System.out.println("Введите элементы массива:");
         /*Пройдёмся по всему массиву, заполняя его*/
         for (int i = 0; i < sizeSecond; i++) {
